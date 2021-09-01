d66493f9230ee58597ad40c7a0bdae724e2f12a7
Lizzy <lebogang@eblocks.co.za>
2021-09-01 16:44:55 +0200

    first addition of project files

---
+++ Lizzy/Lizzy/Controllers/RecipesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lizzy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetDishes()
+        {
+            string[] dishes = { "Burger", "Water", "Sweets", "Mealies" };
+            if (dishes.Any())
+                return NotFound();
+            return Ok(dishes);
+        }
+        [HttpPost]
+        public ActionResult CreateNewRecipes()
+        {
+            return BadRequest();
+        }
+        [HttpDelete]
+        public ActionResult DeleteRecipes()
+        {
+            bool badThingsHappen = false;
+            if (badThingsHappen)
+                return BadRequest();
+            return NoContent();
+        }
+        [HttpPut]
+        public ActionResult UpdateRecipes()
+        {
+            return NotFound();
+        }
+    }
+}
